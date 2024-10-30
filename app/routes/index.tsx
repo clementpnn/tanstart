e23e10e26a8909cae6d48647635eb40b8dd8e07e
clementpnn <clementpnn@users.noreply.github.com>
2024-10-30 22:28:57 +0100

    feat: add POST for test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,19 +1,45 @@
+import * as fs from 'fs'
 import { createFileRoute } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/start'
 
-const getData = createServerFn('GET', () => {
-  return "World"
+const filePath = 'count.txt'
+
+async function readCount() {
+  return parseInt(
+    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
+  )
+}
+
+const getCount = createServerFn('GET', () => {
+  return readCount()
+})
+
+const updateCount = createServerFn('POST', async (formData: FormData) => {
+  const count = await readCount()
+  const addBy = Number(formData.get('addBy'))
+  await fs.promises.writeFile(filePath, `${count + addBy}`)
+  return new Response('ok', { status: 301, headers: { Location: '/' } })
 })
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: async () => await getData(),
+  loader: async () => await getCount(),
 })
 
 function Home() {
   const state = Route.useLoaderData()
 
   return (
-    <h1>Hello {state}</h1>
+    <div>
+      <form
+        action={updateCount.url}
+        method="POST"
+        encType="multipart/form-data"
+      >
+        <input type="number" name="addBy" defaultValue="1" />
+        <button type="submit">Add</button>
+      </form>
+      <pre>{state}</pre>
+    </div>
   )
 }
