e7f53985b160ce8b457f5646866cd2c2f35ad534
clementpnn <clementpnn@users.noreply.github.com>
2024-10-30 22:45:16 +0100

    feat: add pre-rendu with nitro

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from '@tanstack/start/config'
 export default defineConfig({
   server: {
     preset: 'bun',
+    prerender: {
+      routes: ['/'],
+      crawlLinks: true,
+    },
   },
 })
